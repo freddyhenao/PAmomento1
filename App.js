3fa3f49e2de662d01de44415bec00d55d1245c40
Fahl <freddyhenao28.fh@gmail.com>
2020-10-17 23:05:41 -0500

    reinicio

--- App.js
+++
@@ -1,8 +0,0 @@
-// Calcular la fechade naciminto del jugoador
-
-let edad = new Date('12/19/1999');
-let ahora = new Date();
-let agnios = ahora.getFullYear() - edad.getFullYear();
-
-console.log(agnios);
-
