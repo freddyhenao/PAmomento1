13722836e8f367612e3b9a6f7beee3a1bfda438c
Fahl <freddyhenao28.fh@gmail.com>
2020-10-17 19:59:25 -0500

    Agrego bootstrap

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edad Jugador </title>
+</head>
+<body>
+    <div>
+    
+    </div>
+    <table>
+    <tr>
+    <th>Nombre del Jugador </th>
+    <th>Fecha de Nacimiento</th>
+    <th>Edad</th>
+    </tr>
+
+    <tr>
+    <td><input type="text" name="" id="nombre" class="form-control "/>
+    </tr>
+    </table>
+</body>
+</html>
